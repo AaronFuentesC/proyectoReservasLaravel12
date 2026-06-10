a4ae50fb8fa32f64debde0847a01163e5a25de98
AarronFuentesC <aaronfuentescasanova@gmail.com>
2026-06-10 12:13:22 +0200

    fix: Solucionado problema en el que el sidebar aparecía en la vista de los ajustes de un usuario normal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,11 @@
-<x-layouts::app.sidebar :title="$title ?? null">
-    <flux:main>
+@if (auth()->check() && auth()->user()->hasRole('admin'))
+    <x-layouts::app.sidebar :title="$title ?? null">
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+    </x-layouts::app.sidebar>
+@else
+    <x-layouts::app.header>
         {{ $slot }}
-    </flux:main>
-</x-layouts::app.sidebar>
+    </x-layouts::app.header>
+@endif
